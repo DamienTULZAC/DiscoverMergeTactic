bf9a5bcdd8f3b545d4d26b32bab4473704616929
HugoSalaun-dev <hugsalaun@gmail.com>
2025-12-10 16:53:20 +0100

    taille cartes et remove banniere

--- frontend/src/pages/Cards.jsx
+++ frontend/src/pages/Cards.jsx
@@ -87,7 +87,7 @@ export default function Cards() {
 
           <div className="row g-4">
             {filteredCards.map(card => (
-              <div className="col-6 col-md-4 col-lg-3" key={card.id}>
+              <div className="col-6 col-md-4 col-lg-2" key={card.id}>
                 <CardItem card={card} />
               </div>
             ))}
